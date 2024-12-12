b644d1b5b973d8c7d1e1f71d4835d4ad4ee5f3d8
marcostsi <lira.santos@academico.ifpb.edu.br>
2024-12-12 17:15:45 -0300

    Add files via upload

---
+++ Atividade-Lista-de-Tarefa/Tarefa01/script.js
@@ -0,0 +1,9 @@
+const caixadeentrada = document.querySelector(".campodeentrada input");
+const adicionarbutton = document.querySelector(".campodeentrada button");
+const listadetarefas = document.querySelector(".listadetarefas");
+const excluirtudo = document.querySelector(".rodape button");
+
+adicionarbutton.onclick = () => {
+    let valorinserido = caixadeentrada.nodeValue;
+    let dadosdearmazenamentolocal 
+}
